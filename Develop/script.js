4e63b6a2f575aecc5e55f55b0c81174b11c62ce4
Samantha Samarua <samantha.samarua@gmail.com>
2024-01-01 17:35:02 +0930

    03 - Specified Password length

---
+++ Develop/script.js
@@ -0,0 +1,12 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+function generatePassword(length) {
+
+
+// Write password to the #password input
+function writePassword() {
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
